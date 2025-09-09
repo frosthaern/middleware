6437673d566a0e6d6edc0e20056835876a64653a
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2025-09-10 00:09:39 +0530

    [feat]:
    - this is the first simple algorithm
    - check last time the route had a get request
    - if it is greater then x seconds then accept it else reject it
    - simple as asf

--- main.go
+++ main.go
@@ -1,11 +1,47 @@
 package main
 
-import "fmt"
-import "string"
-import "time"
+import (
+	"fmt"
+	"time"
+	"sync"
+	"net/http"
+)
 
-rate_table := make(map[string]time.Time)
+var (
+	
+	route_time_map = make(map[string]time.Time)
+	mu sync.RWMutex
+)
+
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+	var req_url_path = r.URL.Path
+	value, ok := route_time_map[req_url_path]
+	var this_time = time.Now()
+	fmt.Printf("request obj: %s\n", req_url_path);
+	if ok {
+		var lapsed_time = this_time.Second() - value.Second()
+		if lapsed_time < 5 {
+			fmt.Printf("request was rejected at second %d ", time.Now().Second())
+			return
+		} else {
+			route_time_map[req_url_path] = this_time
+			fmt.Printf("this request waxs accepted at second %d", this_time.Second())
+			return
+		}
+	} else {
+		route_time_map[req_url_path] = this_time
+		fmt.Printf("this request waxs accepted at second %d", this_time.Second())
+		return		
+	}
+}
 
 func main() {
-	
+	localhost_addr := ":3000"
+	http.HandleFunc("/", handleRoot);
+	if err := http.ListenAndServe(localhost_addr, nil); err != nil {
+		fmt.Printf("Server Failed to Start at %s", localhost_addr)
+	}
 }
