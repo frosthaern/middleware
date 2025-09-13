497ae6393e225825864901cd3e5c71cfbfffac60
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2025-09-13 19:29:48 +0530

    [feat]: middleware/ratelimiter
    - added token bucket algo for ratelimiting

---
+++ middleware/ratelimiter/tokenBucket.go
@@ -0,0 +1,77 @@
+package ratelimiter
+
+import (
+	middleware "middleware/middleware"
+	"net/http"
+	"time"
+	"sync"
+)
+
+type TokenBucket struct {
+	capacity   int // this is max no of tokens at any point of time
+	tokens     int // no of currently available tokens
+	refillRate int // how many tokens to fill whenever a period happens
+	refillPeriod time.Duration // time interval between each refill
+	lastRefill time.Time
+	mu sync.Mutex
+}
+
+func NewTokenBucket() {
+}
+
+func (t *TokenBucket)RateLimiter() middleware.MiddlewareFunc {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if !t.Allow() {
+				http.Error(w, "too many requests per second", http.StatusTooManyRequests)
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+// refill()
+/*
+ * i hope that when it is initialized it has all the tokens
+ * and the last refill time is set
+ * so that i can always call the refill and it's valid
+ */
+
+func (t *TokenBucket) refill() {
+	prev := t.lastRefill
+	now := time.Now()
+	periods := int(now.Sub(prev) / t.refillPeriod)
+
+	if periods > 0 {
+		t.tokens += t.refillRate * periods
+		t.tokens = min(t.capacity, t.tokens)
+
+		// because if you are in between two time periods, you only fill up till last period
+		// and that's why your lastRefill will also be the last period
+
+		t.lastRefill = t.lastRefill.Add(time.Duration(periods) * t.refillPeriod)
+	}
+}
+
+func (t *TokenBucket) Allow() bool {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	t.refill()
+
+	if t.tokens > 0 {
+		t.tokens--
+		return true
+	}
+	return false
+}
+
+func (t *TokenBucket) GetRemainingTokens() int {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	t.refill()
+
+	return t.tokens
+}
